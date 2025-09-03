ab577e0bafc627ef1390164ed680d4e1291e3d86
NguyenHuyKhoi <huykhoiqc@gmail.com>
2025-09-03 18:22:30 +0700

    update

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,15 +1,15 @@
 /* eslint-disable no-empty-pattern */
-import { Stack } from "@mui/material";
-import { ChannelPage } from "../channels";
-import { useSelector, useUI } from "@common";
-import { VideoDictation } from "../video_dictation";
+import { useUI } from "@common";
+import { Stack, Typography } from "@mui/material";
+import { COLORS } from "@theme";
 
 export function HomePage() {
-  const { focus_video } = useSelector((x) => x.common);
   const {} = useUI();
   return (
     <Stack sx={{ width: "100vw", height: "100vh", backgroundColor: "#fff" }}>
-      {focus_video ? <VideoDictation /> : <ChannelPage />}
+      <Typography style={{ fontSize: "100px", color: COLORS.DarkCharcoal }}>
+        TOEIC PAGE
+      </Typography>
     </Stack>
   );
 }
